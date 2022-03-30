7c3ffc142a6e81e6f78f6dcb69fb81b6e606780c
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-03-30 00:09:29 -0300

    Implemented Select component change of value, created new states for filtering files and changes to show them.

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -17,6 +17,9 @@ const PRODUCT_IMAGES_FOLDER = '/img/products/';
 export default function ProductPage() {
   const { id: productId } = useParams();
   const [product, setProduct] = useState(null);
+  const [filteredProduct, setFilteredProduct] = useState(null);
+  const [selectedFirmwareVersion, setSelectedFirmwareVersion] = useState('');
+  const [selectedFilesVersion, setSelectedFilesVersion] = useState('');
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
@@ -25,10 +28,15 @@ export default function ProductPage() {
       try {
         const apiProduct = await apiGetProductFrom(productId);
         // .sort((a, b) => a.name.localeCompare(b.name));
-        // .map((city) => ({ ...city, description: city.name }));
 
-        if (apiProduct && apiProduct.length > 0) setProduct(apiProduct[0]);
-        else setError('Produto não encontrado!');
+        if (apiProduct && apiProduct.length > 0) {
+          const currentProduct = apiProduct[0];
+          setProduct(currentProduct);
+          setFilteredProduct(Object.assign({}, currentProduct));
+          setSelectedFirmwareVersion(currentProduct.firmare_versions[0]);
+          // op1 = "Todas"
+          setSelectedFilesVersion('op1');
+        } else setError('Produto não encontrado!');
 
         setTimeout(() => {
           setLoading(false);
@@ -41,6 +49,56 @@ export default function ProductPage() {
 
     getProduct();
   }, [productId]);
+
+  useEffect(() => {
+    async function getProductWithFilteredFiles() {
+      if (selectedFirmwareVersion) {
+        const productWithFilteredFiles = Object.assign({}, product);
+        const arrayOfProductFiles = Object.assign([], product.files);
+
+        // Filter files by firmware version
+        const arrayOfFilteredFilesByFirmwareVersion = arrayOfProductFiles.filter((file) =>
+          file.firmare_effectivity.includes(selectedFirmwareVersion)
+        );
+
+        // Filter files by file version (op2 = "Mais recente")
+        if (selectedFilesVersion === 'op2') {
+          const groupBy = function (xs, key) {
+            return xs.reduce(function (rv, x) {
+              (rv[x[key]] = rv[x[key]] || []).push(x);
+              return rv;
+            }, {});
+          };
+
+          const arrayOfGroupedFilesByName = groupBy(arrayOfFilteredFilesByFirmwareVersion, 'name');
+          const arrayOfFilteredFilesByFileVersion = [];
+
+          for (const [_key, value] of Object.entries(arrayOfGroupedFilesByName)) {
+            const max = value.reduce(function (prev, current) {
+              return prev.version_code > current.version_code ? prev : current;
+            });
+            arrayOfFilteredFilesByFileVersion.push(max);
+          }
+
+          productWithFilteredFiles.files = arrayOfFilteredFilesByFileVersion;
+        } else {
+          productWithFilteredFiles.files = arrayOfFilteredFilesByFirmwareVersion;
+        }
+
+        setFilteredProduct(productWithFilteredFiles);
+      }
+    }
+
+    getProductWithFilteredFiles();
+  }, [selectedFirmwareVersion, selectedFilesVersion]);
+
+  const handleFirmwareVersionChange = (newSelectedFirmwareVersion) => {
+    setSelectedFirmwareVersion(newSelectedFirmwareVersion);
+  };
+
+  const handleFilesVersionChange = (newSelectedFilesVersion) => {
+    setSelectedFilesVersion(newSelectedFilesVersion);
+  };
 
   let mainJsx = (
     <div className="flex justify-center my-4">
@@ -55,31 +113,33 @@ export default function ProductPage() {
   if (!loading && !error) {
     mainJsx = (
       <>
-        <h3 className="text-center font-semibold mb-6">{product.name}</h3>
+        <h3 className="text-center font-semibold mb-6">{filteredProduct.name}</h3>
         <div className="max-w-2xl mx-auto p-4 bg-gray-200 rounded-lg shadow-md">
           <div className="flex flex-row mx-auto items-center space-x-2 mb-2">
-            <img src={PRODUCT_IMAGES_FOLDER + product.image} alt="" className="h-48 m-2" />
+            <img src={PRODUCT_IMAGES_FOLDER + filteredProduct.image} alt="" className="h-48 m-2" />
             <div className="flex flex-col space-y-4 p-2">
-              <p>{product.description}</p>
+              <p>{filteredProduct.description}</p>
               <p>
                 <span className="font-semibold">Categoria: </span>
-                {product.category}
+                {filteredProduct.category}
               </p>
               <p>
                 <span className="font-semibold">Modelo: </span>
-                {product.model}
+                {filteredProduct.model}
               </p>
               <Select
                 labelDescription="Versão de Firmware: "
-                // onChangeValue = null
+                onChangeValue={handleFirmwareVersionChange}
+                selectedValue={selectedFirmwareVersion}
               >
-                {product.firmare_versions.map((firm) => {
+                {filteredProduct.firmare_versions.map((firm) => {
                   return { id: firm, description: firm };
                 })}
               </Select>
               <Select
                 labelDescription="Versão dos Arquivos: "
-                // onChangeValue = null
+                onChangeValue={handleFilesVersionChange}
+                selectedValue={selectedFilesVersion}
               >
                 {[
                   { id: 'op1', description: 'Todas' },
@@ -104,7 +164,7 @@ export default function ProductPage() {
               contentOuterClassName="shadow-md"
             >
               <div className="grid grid-cols-5 gap-4 mx-auto">
-                {product.files.map(({ id, name_with_version, archive_name, extension }) => {
+                {filteredProduct.files.map(({ id, name_with_version, archive_name, extension }) => {
                   return (
                     <a
                       key={id}
