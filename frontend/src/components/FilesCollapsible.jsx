2d63c2ebb18e5d99b728d6326d13dce84bea3ae0
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-03-31 21:39:28 -0300

    Created new component for files collapsible, refactored and cleaned up code and improved styling.

---
+++ frontend/src/components/FilesCollapsible.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { AiOutlineFilePdf as FilePdfIcon } from 'react-icons/ai';
+import { AiOutlineFileZip as FileZipIcon } from 'react-icons/ai';
+import { AiOutlineFileUnknown as FileUnknownIcon } from 'react-icons/ai';
+import Collapsible from 'react-collapsible';
+import { BsChevronDown } from 'react-icons/bs';
+
+const PRODUCT_FILES_FOLDER = '/files/products/';
+
+export default function FilesCollapsible({ triggerTitleName = 'Arquivos', children: files = [] }) {
+  return (
+    <Collapsible
+      trigger={
+        <>
+          <p className="font-semibold">{triggerTitleName}</p>
+          <BsChevronDown size="1.1rem" />
+        </>
+      }
+      transitionTime="200"
+      triggerClassName="flex flex-row p-2 items-center justify-between bg-yellow-100 rounded-lg shadow-md"
+      triggerOpenedClassName="flex flex-row p-2 items-center justify-between bg-yellow-100 rounded-t-lg"
+      contentInnerClassName="p-2 rounded-b-lg bg-gray-100"
+      contentOuterClassName="shadow-md"
+    >
+      <div className="grid grid-cols-5 gap-4 mx-auto">
+        {files.map(({ id, name_with_version, archive_name, extension }) => {
+          return (
+            <a
+              key={id}
+              href={PRODUCT_FILES_FOLDER + archive_name}
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col items-center space-y-1 hover:text-yellow-500 hover:scale-105"
+            >
+              {extension.toLowerCase() === 'zip' && <FileZipIcon size="2rem" />}
+              {extension.toLowerCase() === 'pdf' && <FilePdfIcon size="2rem" />}
+              {!['zip', 'pdf'].includes(extension.toLowerCase()) && <FileUnknownIcon size="2rem" />}
+              <span className="text-center ">{name_with_version}</span>
+            </a>
+          );
+        })}
+      </div>
+    </Collapsible>
+  );
+}
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,17 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { AiOutlineFilePdf as FilePdfIcon } from 'react-icons/ai';
-import { AiOutlineFileZip as FileZipIcon } from 'react-icons/ai';
-import { AiOutlineFileUnknown as FileUnknownIcon } from 'react-icons/ai';
-import Collapsible from 'react-collapsible';
-import { BsChevronDown } from 'react-icons/bs';
 
 import Loading from '../components/Loading';
 import Error from '../components/Error';
 import { apiGetProductFrom } from '../services/apiService';
 import Select from '../components/Select';
+import FilesCollapsible from '../components/FilesCollapsible';
 
-const PRODUCT_FILES_FOLDER = '/files/products/';
 const PRODUCT_IMAGES_FOLDER = '/img/products/';
 
 export default function ProductPage() {
@@ -113,10 +108,10 @@ export default function ProductPage() {
   if (!loading && !error) {
     mainJsx = (
       <>
-        <h3 className="text-center font-semibold mb-6">{filteredProduct.name}</h3>
+        <h3 className="text-xl text-center font-semibold mb-6">{filteredProduct.name}</h3>
         <div className="max-w-2xl mx-auto p-4 bg-gray-200 rounded-lg shadow-md">
-          <div className="flex flex-row mx-auto items-center space-x-2 mb-2">
-            <img src={PRODUCT_IMAGES_FOLDER + filteredProduct.image} alt="" className="h-48 m-2" />
+          <div className="flex flex-row mx-auto items-center space-x-4 mb-2">
+            <img src={PRODUCT_IMAGES_FOLDER + filteredProduct.image} alt="" className="h-48 m-4" />
             <div className="flex flex-col space-y-4 p-2">
               <p>{filteredProduct.description}</p>
               <p>
@@ -149,39 +144,15 @@ export default function ProductPage() {
             </div>
           </div>
           <div className="flex flex-col space-y-2">
-            <Collapsible
-              trigger={
-                <>
-                  <p className="font-semibold">Arquivos</p>
-                  <BsChevronDown size="1.1rem" />
-                </>
-              }
-              transitionTime="200"
-              triggerClassName="flex flex-row p-2 items-center justify-between bg-yellow-100 rounded-lg shadow-md"
-              triggerOpenedClassName="flex flex-row p-2 items-center justify-between bg-yellow-100 rounded-t-lg"
-              // contentOuterClassName="border border-black"
-              contentInnerClassName="p-2 rounded-b-lg bg-gray-100"
-              contentOuterClassName="shadow-md"
-            >
-              <div className="grid grid-cols-5 gap-4 mx-auto">
-                {filteredProduct.files.map(({ id, name_with_version, archive_name, extension }) => {
-                  return (
-                    <a
-                      key={id}
-                      href={PRODUCT_FILES_FOLDER + archive_name}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="flex flex-col items-center space-y-1 hover:text-yellow-500 hover:scale-105"
-                    >
-                      {extension.toLowerCase() === 'zip' && <FileZipIcon size="2rem" />}
-                      {extension.toLowerCase() === 'pdf' && <FilePdfIcon size="2rem" />}
-                      {!['zip', 'pdf'].includes(extension.toLowerCase()) && <FileUnknownIcon size="2rem" />}
-                      <span className="text-center ">{name_with_version}</span>
-                    </a>
-                  );
-                })}
-              </div>
-            </Collapsible>
+            <FilesCollapsible triggerTitleName="Documentação">
+              {filteredProduct.files.filter((file) => file.category === 'Documentação')}
+            </FilesCollapsible>
+            <FilesCollapsible triggerTitleName="Recursos de Desenvolvimento">
+              {filteredProduct.files.filter((file) => file.category === 'Recursos de Desenvolvimento')}
+            </FilesCollapsible>
+            <FilesCollapsible triggerTitleName="Outros">
+              {filteredProduct.files.filter((file) => file.category === 'Outros')}
+            </FilesCollapsible>
           </div>
         </div>
       </>
