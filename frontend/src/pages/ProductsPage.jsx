2b4d4f3b35d441356eb440e35daba491c2c55311
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-04-02 19:35:06 -0300

    Added constant for product path and replaced in code

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import Loading from '../components/utils/Loading';
 import { apiGetAllProducts } from '../services/apiService';
 
+const PRODUCT_PATH = '/products/';
 const PRODUCT_IMAGES_FOLDER = '/img/products/';
 
 export default function ProductsPage() {
@@ -34,13 +35,13 @@ export default function ProductsPage() {
     mainJsx = (
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mx-auto">
         {products.map(({ id, name, image }) => {
-          const path = '/products/';
           return (
             <NavLink
               key={id}
               exact
-              to={path + id}
-              className="w-56 p-4 bg-gray-200 rounded-lg shadow-md hover:text-yellow-500 hover:scale-105"
+              to={PRODUCT_PATH + id}
+              className="w-56 p-4 bg-gray-200 rounded-lg shadow-md 
+              hover:text-yellow-500 hover:scale-105"
             >
               <div className="flex flex-col items-center space-y-1">
                 <img src={PRODUCT_IMAGES_FOLDER + image} alt="" className="h-24 m-4" />
