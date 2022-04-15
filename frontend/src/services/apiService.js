2d3d4faddcf6f42bc24a5f9ed32b6f47d3b3a7d9
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-04-14 21:41:53 -0300

    Created new service to get service from id

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -14,3 +14,8 @@ export async function apiGetAllServices() {
   const { data } = await axios.get('http://localhost:3001/services');
   return data;
 }
+
+export async function apiGetServiceFrom(serviceId) {
+  const { data } = await axios.get(`http://localhost:3001/services?id=${serviceId}`);
+  return data;
+}
