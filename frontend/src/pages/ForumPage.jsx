617968200a1d3a3fc072be554e0dc264b2c40274
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-04-26 21:25:54 -0300

    Implemented Forum Page

--- frontend/src/pages/ForumPage.jsx
+++ frontend/src/pages/ForumPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Loading from '../components/utils/Loading';
+
+const FORUM_SRC_URL = 'https://mvp-dfs-igti-dev-team.tribeplatform.com/collection/GRpKVJSNbMmX?layout=basic';
 
 export default function ForumPage() {
+  const [loading, setLoading] = useState(true);
+
+  const handleIframeLoad = () => {
+    setLoading(false);
+  };
+
   return (
-    <div className="flex flex-col space-y-2 p-4">
-      <h3 className="text-center font-semibold mb-6">Fórum do Portal para Desenvolvedores</h3>
+    <div className="flex flex-col space-y-2 max-w-6xl mx-auto p-4">
+      {loading && (
+        <div className="flex justify-center my-4">
+          <Loading />
+        </div>
+      )}
+      <iframe
+        src={FORUM_SRC_URL}
+        name="iframe_a"
+        className="h-screen w-full"
+        title="Iframe Knowledge Base"
+        onLoad={handleIframeLoad}
+      />
     </div>
   );
 }
