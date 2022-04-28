91d0b93153d512ca6bdbe73f7368be384322f7c8
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-04-27 22:10:11 -0300

    Minor changes to Input component

--- frontend/src/components/utils/Input.jsx
+++ frontend/src/components/utils/Input.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-export default function Input({ id = 'id', type = 'text', labelName = 'label' }) {
+export default function Input({ id = 'id', type = 'text', labelName = 'label', textPlaceHolder = 'placeholder' }) {
   return (
     <div>
-      <label htmlFor={id}>{labelName}</label>
+      <label htmlFor={id} className="font-semibold">
+        {labelName}
+      </label>
       <input
         type={type}
         className="mt-2 mb-4 p-2 block w-full rounded-md bg-gray-50"
         id={id}
         required
+        placeholder={textPlaceHolder}
         // value={grade.name}
         // onChange={handleInputChange}
-        id={id}
       />
     </div>
   );
