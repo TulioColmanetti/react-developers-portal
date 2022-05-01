37e323830396505ad98f0e0520fce4e2e0d3bd4e
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-05-01 16:08:35 -0300

    Removed unnecessary services

--- backend/routes/devPortalRoutes.js
+++ backend/routes/devPortalRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { studentModel } from '../models/student.js';
 import { productModel } from '../models/product.js';
 import { serviceModel } from '../models/service.js';
 import { supportModel } from '../models/support.js';
@@ -33,7 +32,7 @@ app.get('/products/:id', async (req, res) => {
 
 //---- Services domain services
 app.get('/services', async (req, res) => {
-  const services = await productModel.find({});
+  const services = await serviceModel.find({});
 
   try {
     res.send(services);
@@ -57,67 +56,12 @@ app.get('/services/:id', async (req, res) => {
 });
 
 //---- Support domain services
-app.get('/support', async (req, res) => {
-  const support = await supportModel.find({});
-
-  try {
-    res.send(support);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
 app.post('/support', async (req, res) => {
   const support = new supportModel(req.body);
 
   try {
     await support.save();
     res.status(201).send(support);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.get('/student', async (req, res) => {
-  const student = await studentModel.find({});
-
-  try {
-    res.send(student);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.post('/student', async (req, res) => {
-  const student = new studentModel(req.body);
-
-  try {
-    await student.save();
-    res.send(student);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.delete('/student/:id', async (req, res) => {
-  try {
-    const student = await studentModel.findOneAndDelete(req.params.id);
-
-    if (!student) {
-      res.status(404).send('Documento nao encontrado');
-    }
-
-    res.status(200).send();
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.patch('/student/:id', async (req, res) => {
-  try {
-    const student = await studentModel.findOneAndUpdate(req.params.id, req.body, { new: true });
-
-    res.send(student);
   } catch (err) {
     res.status(500).send(err);
   }
